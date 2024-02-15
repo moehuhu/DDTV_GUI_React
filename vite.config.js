6da45896995c2c4cb6454c7e25be2baa83c6cd6a
雪糊糊 <994458697@qq.com>
2024-02-15 13:42:10 +0800

    Room List, legacy

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react-swc'
 import svgr from "vite-plugin-svgr";
-
+import legacy from '@vitejs/plugin-legacy'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    legacy(),
+    react(),
+    svgr()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -24,5 +28,5 @@ export default defineConfig({
         changeOrigin: true
       }
     }
-  }
+  },
 })
