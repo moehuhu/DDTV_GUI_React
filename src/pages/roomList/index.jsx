aba02f7d77efe363d7474f177e950be571fe1ed3
雪糊糊 <994458697@qq.com>
2024-02-18 21:32:09 +0800

    小修

--- src/pages/roomList/index.jsx
+++ src/pages/roomList/index.jsx
@@ -58,7 +58,7 @@ const Rooms = () => {
     total={total}
     onChange={(current) => { setPageState({ current }) }}
     pageSizeOptions={[4, 8, 12, 16, 24, 32, 48, 64]}
-    onShowSizeChange={(current, size) => { setPageSize(size); refreshPage() }}
+    onShowSizeChange={(current, size) => { setPageSize(size); setPageState({ current }) }}
     showSizeChanger
     showQuickJumper
   />
