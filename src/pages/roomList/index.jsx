1e91e90617903d82008aedc9b4b8b40d8a0b206e
雪糊糊 <994458697@qq.com>
2024-02-26 20:24:41 +0800

    适配带鱼屏

--- src/pages/roomList/index.jsx
+++ src/pages/roomList/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useAsyncEffect, useInterval } from 'ahooks';
 import useUrlState from '@ahooksjs/use-url-state';
-import { List, Pagination, message } from 'antd';
+import { List, Pagination, message, ConfigProvider } from 'antd';
 import RoomCard from './RoomCard/index';
 import AddRoomModal from './AddRoomModal';
 import SetRoomModal from './SetRoomModal';
@@ -57,20 +57,44 @@ const Rooms = () => {
     pageSize={pageSize}
     total={total}
     onChange={(current) => { setPageState({ current }) }}
-    pageSizeOptions={[4, 8, 12, 16, 24, 32, 48, 64]}
+    pageSizeOptions={[8, 12, 16, 20, 24, 32, 40, 48, 56, 64, 80]}
     onShowSizeChange={(current, size) => { setPageSize(size); setPageState({ current }) }}
     showSizeChanger
     showQuickJumper
   />
-  const grid = { xs: 1, sm: 1, md: 1, lg: 2, xl: 3, xxl: 4, gutter: 16 }
-  const list = <List
-    grid={grid}
-    header={header}
-    renderItem={renderItem}
-    footer={footer}
-    bordered={false}
-    dataSource={roomInfoList}
-  />
+  const grid = { xs: 1, sm: 1, md: 2, lg: 3, xl: 4, xxl: 5, gutter: 16 }
+  const list = <ConfigProvider
+    theme={{
+      token: {
+        screenXXL: 2000,
+        screenXXLMin: 2000,
+        screenXLMax: 1999,
+        screenXL: 1600,
+        screenXLMin: 1600,
+        screenLGMax: 1599,
+        screenLG: 1200,
+        screenLGMin: 1200,
+        screenMDMax: 1199,
+        screenMD: 992,
+        screenMDMin: 992,
+        screenSMMax: 991,
+        screenSM: 768,
+        screenSMMin: 768,
+        screenXSMax: 767,
+        screenXS: 576,
+        screenXSMin: 576,
+      },
+    }}
+  >
+    <List
+      grid={grid}
+      header={header}
+      renderItem={renderItem}
+      footer={footer}
+      bordered={false}
+      dataSource={roomInfoList}
+    />
+  </ConfigProvider>
 
   return <div className='room-list'>
     {contextHolder}
