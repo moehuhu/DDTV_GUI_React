8184b3298cf6eb1fbed7f2a57bc9850bdcc4fca1
雪糊糊 <994458697@qq.com>
2024-02-28 21:30:12 +0800

    手搓表

---
+++ src/pages/batchOperation/stagedRoomInfo.jsx
@@ -0,0 +1,36 @@
+import { Avatar, List, theme } from "antd"
+import { useTranslation } from "react-i18next"
+const { Item } = List
+const StagedRoomInfo = ({ item, selected, extra, onClick, onDoubleClick }) => {
+    const { token } = theme.useToken()
+    const { roomInfo, userInfo } = item
+    const { t } = useTranslation()
+    const noAvatar = 'https://i0.hdslb.com/bfs/face/member/noface.jpg@52w_52h_1c_1s.webp'
+    const avatar = <Avatar className="avatar" src={roomInfo?.face || noAvatar} />
+    const name = <h4
+        className="name"
+        style={{ lineHeight: token.lineHeight, color: token.colorText }}>
+        {userInfo?.name}
+    </h4>
+    const description = <span
+        className="description"
+        style={{ lineHeight: token.lineHeight, color: token.colorTextTertiary }}>
+        {`UID: ${userInfo?.uid}    ${t('roomID')}: ${roomInfo?.roomId}`}
+    </span>
+    const user = <div className="user">
+        {avatar}
+        <div className="info">
+            {name}
+            {description}
+        </div>
+    </div>
+    return <div
+        style={{ background: selected ? token.colorBgTextHover : undefined }}
+        onClick={onClick}
+        onDoubleClick={onDoubleClick}
+        className={`room-info`}>
+        {user}
+        {extra}
+    </div>
+}
+export default StagedRoomInfo
--- src/pages/batchOperation/targetList.jsx
+++ src/pages/batchOperation/targetList.jsx
@@ -1,9 +1,10 @@
-import { List } from "antd"
+import { theme } from "antd"
 import { CloseOutlined } from "@ant-design/icons"
 import { useTranslation } from "react-i18next"
-import RoomInfo from "./roomInfo"
+import StagedRoomInfo from "./stagedRoomInfo"
 import _ from 'lodash'
 const TargetList = (props) => {
+    const { token } = theme.useToken()
     const { setStagedItems, stagedItems = [], stagedMap = {} } = props
     const staged = item => stagedMap[item?.userInfo?.uid]
     const removeItem = item => items => items?.filter(({ userInfo }) => userInfo?.uid != item?.userInfo?.uid)
@@ -12,17 +13,25 @@ const TargetList = (props) => {
         setStagedItems(removeItem(item))
     }
     const removeIcon = item => <CloseOutlined onClick={() => removeStaged(item)} />
-    const renderItem = item => <RoomInfo
+    const renderItem = item => <StagedRoomInfo
+        key={item?.userInfo?.uid}
         onDoubleClick={() => removeStaged(item)}
         item={item}
         extra={removeIcon(item)}
     />
     const { t } = useTranslation()
-    const header = <span>{`${t('Selected')}: ${_.size(stagedItems) || 0}`}</span>
-    return <List bordered
-        header={header}
+    const header = <div className="header" style={{ borderBlockEnd: `1px solid ${token.colorBorderSecondary}` }}>
+        <span>{`${t('Selected')}: ${_.size(stagedItems) || 0}`}</span>
+    </div>
+    const list = <div
         className="target-list"
-        dataSource={stagedItems}
-        renderItem={renderItem} />
+        style={{
+            border: `1px solid ${token.colorBorder}`,
+            borderRadius: token.borderRadiusLG
+        }}>
+        {header}
+        <div className="list">{stagedItems.map(renderItem)}</div>
+    </div>
+    return list
 }
 export default TargetList
