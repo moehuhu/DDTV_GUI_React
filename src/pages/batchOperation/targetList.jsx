f3d2434196b6888ed0bfd52e0cb5c14bb780f51e
雪糊糊 <994458697@qq.com>
2024-02-28 21:44:45 +0800

    预制菜虚拟滚动

--- src/pages/batchOperation/targetList.jsx
+++ src/pages/batchOperation/targetList.jsx
@@ -1,11 +1,15 @@
+import { useRef } from "react"
 import { theme } from "antd"
 import { CloseOutlined } from "@ant-design/icons"
+import { useVirtualList } from "ahooks"
 import { useTranslation } from "react-i18next"
 import StagedRoomInfo from "./stagedRoomInfo"
 import _ from 'lodash'
 const TargetList = (props) => {
     const { token } = theme.useToken()
     const { setStagedItems, stagedItems = [], stagedMap = {} } = props
+    const containerRef = useRef(null);
+    const wrapperRef = useRef(null);
     const staged = item => stagedMap[item?.userInfo?.uid]
     const removeItem = item => items => items?.filter(({ userInfo }) => userInfo?.uid != item?.userInfo?.uid)
     const removeStaged = (item) => {
@@ -23,15 +27,23 @@ const TargetList = (props) => {
     const header = <div className="header" style={{ borderBlockEnd: `1px solid ${token.colorBorderSecondary}` }}>
         <span>{`${t('Selected')}: ${_.size(stagedItems) || 0}`}</span>
     </div>
-    const list = <div
+    const [list] = useVirtualList(stagedItems, {
+        containerTarget: containerRef,
+        wrapperTarget: wrapperRef,
+        itemHeight: 75,
+        overscan: 10,
+    })
+    const targetList = <div
         className="target-list"
         style={{
             border: `1px solid ${token.colorBorder}`,
             borderRadius: token.borderRadiusLG
         }}>
         {header}
-        <div className="list">{stagedItems.map(renderItem)}</div>
+        <div ref={containerRef} className="list">
+            <div ref={wrapperRef}>{list.map(ele => renderItem(ele.data))}</div>
+        </div>
     </div>
-    return list
+    return targetList
 }
 export default TargetList
