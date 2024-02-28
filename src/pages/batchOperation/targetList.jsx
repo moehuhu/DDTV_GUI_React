b9bdc13a8f4d3e9fcdfaeb9b917dea7eb9601dd4
雪糊糊 <994458697@qq.com>
2024-02-28 20:18:48 +0800

    selected

--- src/pages/batchOperation/targetList.jsx
+++ src/pages/batchOperation/targetList.jsx
@@ -1,5 +1,8 @@
 import { List } from "antd"
+import { CloseOutlined } from "@ant-design/icons"
+import { useTranslation } from "react-i18next"
 import RoomInfo from "./roomInfo"
+import _ from 'lodash'
 const TargetList = (props) => {
     const { setStagedItems, stagedItems = [], stagedMap = {} } = props
     const staged = item => stagedMap[item?.userInfo?.uid]
@@ -8,9 +11,18 @@ const TargetList = (props) => {
         if (!staged(item)) { return }
         setStagedItems(removeItem(item))
     }
+    const removeIcon = item => <CloseOutlined onClick={() => removeStaged(item)} />
+    const renderItem = item => <RoomInfo
+        onDoubleClick={() => removeStaged(item)}
+        item={item}
+        extra={removeIcon(item)}
+    />
+    const { t } = useTranslation()
+    const header = <span>{`${t('Selected')}: ${_.size(stagedItems) || 0}`}</span>
     return <List bordered
+        header={header}
         className="target-list"
         dataSource={stagedItems}
-        renderItem={item => <RoomInfo onDoubleClick={() => removeStaged(item)} item={item} />} />
+        renderItem={renderItem} />
 }
 export default TargetList
