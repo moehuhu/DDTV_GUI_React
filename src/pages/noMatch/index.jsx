27a9de4d729cc0652f1a928b708bc2fbf020a530
moehuhu <994458697@qq.com>
2024-03-13 14:15:44 +0800

    home按钮

--- src/pages/noMatch/index.jsx
+++ src/pages/noMatch/index.jsx
@@ -1,17 +1,29 @@
-import { Image, theme } from "antd"
 import NotFound from "../../../public/not_found.png"
+import { Image, Button, theme } from "antd"
+import { HomeFilled } from "@ant-design/icons"
+
 const NoMatch = () => {
   const { token } = theme.useToken()
   return <div
     className="no-match"
     style={{
       display: 'flex',
+      flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
       height: "100vh",
       background: token.colorBgContainer
     }}>
-    <Image preview={false} src={NotFound} />
+    <Image
+      preview={false}
+      src={NotFound}
+    />
+    <Button
+      onClick={() => window.location.href = "/"}
+      icon={<HomeFilled />}
+      type="primary"
+      style={{ margin: token.marginLG }}
+    />
   </div>
 }
 export default NoMatch
