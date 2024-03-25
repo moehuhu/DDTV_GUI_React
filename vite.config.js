0a10d98065eb47e8f4416a058a64df9b56ac4785
moehuhu <994458697@qq.com>
2024-03-25 15:16:55 +0800

    ws开发服务器代理

--- vite.config.js
+++ vite.config.js
@@ -26,6 +26,11 @@ export default defineConfig({
       '/rec_file': {
         target: 'http://127.0.0.1:11419/',
         changeOrigin: true
+      },
+      '/ws': {
+        target: 'ws://127.0.0.1:11419/',
+        changeOrigin: true,
+        ws: true
       }
     }
   },
