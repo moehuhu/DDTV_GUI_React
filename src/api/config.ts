658ba35c427b3a6690000c79bb9b0c671e94ca86
moehuhu <994458697@qq.com>
2024-03-26 23:36:39 +0800

    添加接口，这盘菜微软端翻了三次，主厨端翻了两次

--- src/api/config.ts
+++ src/api/config.ts
@@ -5,6 +5,10 @@ export function reloadConfig() {
   return instance.post<Response<any>>('config/reload_configuration')
 }
 
+export function resetConfig() {
+  return instance.get<Response<any>>('config/restore_all_settings_to_default')
+}
+
 export function setRecordingPath(data: { path?: string; check?: string }) {
   return instance.post<Response<any>>('config/set_recording_path', data)
 }
@@ -19,4 +23,24 @@ export function setFileNameAndPath(data: { path_and_format?: string; check?: str
 
 export function getFileNameAndPath() {
   return instance.get<Response<any>>('config/get_default_file_path_name_format')
-} 
+}
+
+export function getHLSWaitingTime() {
+  return instance.get<Response<any>>('config/get_hls_waiting_time')
+}
+
+export function setHLSWaitingTime(data: { time: bigint; }) {
+  return instance.post<Response<any>>('config/set_hls_waiting_time', data)
+}
+
+export function getAutoRepair() {
+  return instance.get<Response<any>>('config/get_automatic_repair')
+}
+
+export function setAutoRepair(data: { automatic_repair: boolean; }) {
+  return instance.post<Response<any>>('config/set_automatic_repair', data)
+}
+
+export function getCoreVersion() {
+  return instance.get<Response<any>>('config/get_core_version')
+}
