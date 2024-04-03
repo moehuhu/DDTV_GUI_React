6545e3add6506fb59a89319bb1553158ffac0dd8
moehuhu <994458697@qq.com>
2024-04-04 01:19:19 +0800

    我又改了

--- src/hooks/useWebSocketMessage.jsx
+++ src/hooks/useWebSocketMessage.jsx
@@ -1,15 +1,32 @@
-const url = new URL(window.location.href)
-const protocol = url.protocol == 'https:' ? 'wss' : 'ws'
-const wsURL = new URL(url.origin)
-wsURL.protocol = protocol
-wsURL.pathname = '/ws'
-import { useState } from 'react'
-import { useWebSocket } from 'ahooks'
-import { singletonHook } from 'react-singleton-hook';
+import { useState, useEffect } from 'react'
 
-const useWebSocketMessage = singletonHook(wsURL, (options) => {
-  const [ws] = useState(wsURL)
-  const result = useWebSocket(ws, options)
-  return result
-})
+const useWebSocketMessage = () => {
+  const [onOpen, setOpenCallback] = useState()
+  const [onMessage, setMessageCallback] = useState()
+  const [onClose, setCloseCallback] = useState()
+  const toData = message => JSON.parse(message.data)
+
+  useEffect(() => {
+    const url = new URL(window.location.href)
+    const protocol = url.protocol == 'https:' ? 'wss' : 'ws'
+    const wsURL = new URL(url.origin)
+    wsURL.protocol = protocol
+    wsURL.pathname = '/ws'
+    const newSocket = new WebSocket(wsURL);
+    newSocket.onopen = e => onOpen?.(toData(e))
+    newSocket.onmessage = e => onMessage?.(toData(e))
+    newSocket.onclose = e => onClose?.(toData(e))
+    return () => newSocket.close()
+  }, [onOpen, onMessage, onClose]);
+
+  const open = fn => setOpenCallback(() => fn)
+  const message = fn => setMessageCallback(() => fn)
+  const close = fn => setCloseCallback(() => fn)
+
+  const addEventListener = (type, callback) => {
+    const listenTo = { open, message, close }[type]
+    listenTo?.(callback)
+  }
+  return { addEventListener }
+}
 export default useWebSocketMessage
