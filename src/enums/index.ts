35840b3a251d1a8f50a71c130e619de05e928d0a
moehuhu <994458697@qq.com>
2024-04-04 03:45:41 +0800

    opcode

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,5 +1,6 @@
 import SearchType from './search_type'
 import LoginStatus from './login_state'
 import Code from './code'
+import Opcode from './opcode'
 
-export { LoginStatus, Code, SearchType }
+export { LoginStatus, Code, SearchType, Opcode }
