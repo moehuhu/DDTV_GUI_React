66d286fe25bd5ab23339cdfbdee713ca244abcf5
moehuhu <994458697@qq.com>
2024-04-04 14:36:51 +0800

    参数全放worker

--- src/hooks/useWebSocketMessage.jsx
+++ src/hooks/useWebSocketMessage.jsx
@@ -5,12 +5,6 @@ const useWebSocketMessage = () => {
 
   useEffect(() => {
     const workerInstance = new Worker(new URL('../utils/webSocketWorker.js', import.meta.url))
-    const url = new URL(window.location.href)
-    const protocol = url.protocol == 'https:' ? 'wss' : 'ws'
-    const wsURL = new URL(url.origin)
-    wsURL.protocol = protocol
-    wsURL.pathname = '/ws'
-    workerInstance.postMessage({ type: 'connect', payload: wsURL.toString() })
     workerInstance.addEventListener('message', e => onMessage?.(e?.data))
     return () => workerInstance.postMessage({ type: 'close' })
   }, [onMessage]);
--- src/utils/webSocketWorker.js
+++ src/utils/webSocketWorker.js
@@ -1,13 +1,13 @@
-let socket = null
+const url = new URL(location.href)
+const protocol = url.protocol == 'https:' ? 'wss' : 'ws'
+const wsURL = new URL(url.origin)
+wsURL.protocol = protocol
+wsURL.pathname = '/ws'
 const toData = message => JSON.parse(message.data)
-
-const connect = (wsURL) => {
-  socket = new WebSocket(wsURL);
-  socket.addEventListener('message', event => self.postMessage(toData(event)))
-}
+const socket = new WebSocket(wsURL);
+socket.addEventListener('message', event => self.postMessage(toData(event)))
 self.addEventListener('message', ({ data }) => {
   const func = {
-    'connect': () => connect(data?.payload),
     'close': () => socket?.readyState === 1 && socket?.close?.()
   }[data?.type]
   func?.()
