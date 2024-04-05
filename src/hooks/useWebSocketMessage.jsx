a2e7f43b26a87743d6085f6be8e019407eb6dccb
moehuhu <994458697@qq.com>
2024-04-06 01:01:11 +0800

    扬了别的，专门用mitt

--- src/hooks/useWebSocketMessage.jsx
+++ src/hooks/useWebSocketMessage.jsx
@@ -1,19 +1,15 @@
 import { useContext } from 'react'
-import { useSetState } from 'ahooks'
 import { WebSocketContext } from '../WebSocketProvider'
-import _ from 'lodash'
 
 const useWebSocketMessage = () => {
-  const [callbacks, setMessageCallback] = useSetState({})
   const { emitter } = useContext(WebSocketContext)
   const addEventListener = (type, callback) => {
-    setMessageCallback({ [type]: callback })
     emitter.on(type, callback)
   }
-  const removeEventListener = (type) => {
-    setMessageCallback(_.omit(callbacks, type))
-    emitter.off(type, callbacks[type])
+  const removeEventListener = (type, callback) => {
+    emitter.off(type, callback)
   }
   return { addEventListener, removeEventListener }
 }
+
 export default useWebSocketMessage
