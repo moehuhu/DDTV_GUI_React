f0a179a5e2d55d1f66ced5d90ff047f46b14df06
moehuhu <994458697@qq.com>
2024-04-06 01:20:53 +0800

    小改改

--- src/WebSocketProvider.jsx
+++ src/WebSocketProvider.jsx
@@ -1,14 +1,12 @@
 import { createContext, useEffect, useMemo } from 'react';
 import mitt from 'mitt'
+const workerURL = new URL('./utils/webSocketWorker.js', import.meta.url)
 export const WebSocketContext = createContext();
 const WebSocketProvider = ({ children }) => {
   const emitter = useMemo(() => mitt(), [])
   useEffect(() => {
-    const workerInstance = new Worker(new URL('./utils/webSocketWorker.js', import.meta.url), { type: 'module' })
-    workerInstance.postMessage({ type: 'connect' })
-    workerInstance.addEventListener('message', e => {
-      emitter.emit('message', e?.data)
-    })
+    const workerInstance = new Worker(workerURL, { type: 'module' })
+    workerInstance.addEventListener('message', e => emitter.emit('message', e?.data))
     return () => workerInstance.postMessage({ type: 'close' })
   }, [emitter])
   return <WebSocketContext.Provider value={{ emitter }}>
