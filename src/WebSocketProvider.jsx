af055be5967df3dd568ce98409dc089fab274ee8
moehuhu <994458697@qq.com>
2024-04-06 00:51:04 +0800

    mitt真好用

--- src/WebSocketProvider.jsx
+++ src/WebSocketProvider.jsx
@@ -1,21 +1,17 @@
-import { createContext, useState, useEffect, useMemo } from 'react';
+import { createContext, useEffect, useMemo } from 'react';
 import mitt from 'mitt'
-
 export const WebSocketContext = createContext();
-
 const WebSocketProvider = ({ children }) => {
-  const [message, setMessage] = useState({});
   const emitter = useMemo(() => mitt(), [])
   useEffect(() => {
     const workerInstance = new Worker(new URL('./utils/webSocketWorker.js', import.meta.url))
     workerInstance.postMessage({ type: 'connect' })
     workerInstance.addEventListener('message', e => {
-      setMessage(e?.data)
       emitter.emit('message', e?.data)
     })
     return () => workerInstance.postMessage({ type: 'close' })
   }, [emitter])
-  return <WebSocketContext.Provider value={{ message, emitter }}>
+  return <WebSocketContext.Provider value={{ emitter }}>
     {children}
   </WebSocketContext.Provider>
 };
--- src/utils/webSocketWorker.js
+++ src/utils/webSocketWorker.js
@@ -5,22 +5,12 @@ wsURL.protocol = protocol
 wsURL.pathname = '/ws'
 const toData = message => JSON.parse(message.data)
 const socket = new WebSocket(wsURL);
-const events = []
-socket.addEventListener('message', event => events.push(event))
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
-const postMessage = async () => {
-  while (events.length > 0) {
-    const event = events.shift()
-    const data = toData(event)
-    self.postMessage(data)
-    await delay(100)
-  }
+socket.addEventListener('message', event => postMessage(toData(event)))
+const close = () => {
+  socket?.readyState === 1 && socket?.close?.()
+  self.close()
 }
-const post = () => setInterval(postMessage, 100)
 self.addEventListener('message', ({ data }) => {
-  const func = {
-    'connect': () => post(),
-    'close': () => socket?.readyState === 1 && socket?.close?.()
-  }[data?.type]
+  const func = { close }[data?.type]
   func?.()
 })
