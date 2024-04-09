8d60916f75f56889b9a4d66846278eec81d0b5fc
moehuhu <994458697@qq.com>
2024-04-09 23:46:57 +0800

    用进度条表示内存占用和存储占用

--- src/SystemResource.jsx
+++ src/SystemResource.jsx
@@ -1,6 +1,6 @@
 import useSystemSource from './hooks/useSystemResource';
 import { useRafInterval } from 'ahooks';
-import { theme } from 'antd';
+import { theme, Progress, Tooltip } from 'antd';
 import { useTranslation } from 'react-i18next';
 import bytes from 'bytes';
 const SystemResource = () => {
@@ -12,10 +12,25 @@ const SystemResource = () => {
   const occupiedMem = totalMem - (systemState?.Memory?.Available || 0)
   const totalMemory = bytes(totalMem, { unitSeparator: ' ' })
   const occupiedMemory = bytes(occupiedMem, { unitSeparator: ' ' })
-  const memory = <span>{`${t('Memory')}: ${occupiedMemory} / ${totalMemory}`}</span>
+  const usedMemoryPercent = Math.round((occupiedMem / totalMem) * 100)
+  const usedMemoryColor = usedMemoryPercent > 90 ? token.colorError : usedMemoryPercent > 80 ? token.colorWarning : token.colorPrimary
+  const memory = <Tooltip title={`${t('Memory')}: ${occupiedMemory} / ${totalMemory}`}>
+    <Progress
+      percent={usedMemoryPercent}
+      format={(percent) => <span style={{ color: usedMemoryColor }}>{percent}%</span>}
+      strokeColor={usedMemoryColor}
+      size="small" />
+  </Tooltip>
   const totalDisk = systemState?.HDDInfo?.[0]?.Size
   const occupiedDisk = systemState?.HDDInfo?.[0]?.Usage
-  const disk = <span>{`${t('Disk')}: ${occupiedDisk} / ${totalDisk}`}</span>
+  const usedDiskPercent = parseInt((systemState?.HDDInfo?.[0]?.Used || '').replace('%', ''))
+  const usedDiskColor = usedDiskPercent > 90 ? token.colorError : usedDiskPercent > 80 ? token.colorWarning : token.colorPrimary;
+  const disk = <Tooltip title={`${t('Disk')}: ${occupiedDisk} / ${totalDisk}`}>
+    <Progress
+      strokeColor={usedDiskColor}
+      format={(percent) => <span style={{ color: usedDiskColor }}>{percent}%</span>}
+      percent={usedDiskPercent} size="small" />
+  </Tooltip>
   return <div className='system-bar' style={{ margin: token.margin }}>
     {memory}
     {disk}
