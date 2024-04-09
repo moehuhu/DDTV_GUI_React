0e58693bfe1d6f01157d17c4b8f71a185a4c244b
moehuhu <994458697@qq.com>
2024-04-09 21:45:17 +0800

    开播状态用ws连接检测

--- src/hooks/useRecordTask.jsx
+++ src/hooks/useRecordTask.jsx
@@ -7,19 +7,31 @@ import useWebSocketMessage from './useWebSocketMessage'
 const useRecordTask = (onLoadingEnd) => {
     const [isLoading, { setTrue, setFalse }] = useBoolean()
     const socket = useWebSocketMessage()
-
-    const createRecTask = async (uid) => {
-        setTrue()
-        const [err, res] = await to(createTask({ uid, state: true }))
+    const stopLoadingAndRefresh = () => {
         setFalse()
         onLoadingEnd?.()
+    }
+    const createRecTask = async (uid) => {
+        const checkAddSuccess = (data) => {
+            const isThisTask = data?.UID == uid
+            if (!isThisTask) { return }
+            const isDownload = data?.DownInfo?.IsDownload
+            if (!isDownload) { stopLoadingAndRefresh() }
+            socket.removeEventListener(Opcode.SuccessfullyAddedRecordingTask, checkAddSuccess)
+        }
+        const checkRecordingStart = (data) => {
+            const isThisTask = data?.UID == uid
+            if (!isThisTask) { return }
+            stopLoadingAndRefresh()
+            socket.removeEventListener(Opcode.StartRecording, checkRecordingStart)
+        }
+        socket.addEventListener(Opcode.SuccessfullyAddedRecordingTask, checkAddSuccess)
+        socket.addEventListener(Opcode.StartRecording, checkRecordingStart)
+        setTrue()
+        const [err, res] = await to(createTask({ uid, state: true }))
         return [err, res]
     }
     const cancelRecTask = async (uid) => {
-        const stopLoadingAndRefresh = () => {
-            setFalse()
-            onLoadingEnd?.()
-        }
         const checkCancelSuccess = (data) => {
             const isThisTask = data?.UID == uid
             if (!isThisTask) { return }
