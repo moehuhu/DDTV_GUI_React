d157efad014eb73098f1811b6fa3cd73ef54ac56
moehuhu <994458697@qq.com>
2024-04-12 16:05:21 +0800

    调样式

--- src/pages/batchOperation/OriginList/index.jsx
+++ src/pages/batchOperation/OriginList/index.jsx
@@ -141,8 +141,9 @@ const OriginList = (props) => {
         style={{
             border: `1px solid ${token.colorBorder}`,
             borderRadius: token.borderRadiusLG,
-            height: responsive.md ? 'calc(100% - 64px)' : 'calc(48%)',
-            width: responsive.md ? 'calc(50% - 48px)' : 'calc(100% - 32px)'
+            height: responsive.md ? 'calc(100% - 64px)' : 'calc(800px - 64px)',
+            margin: responsive.md ? undefined : '32px 0px',
+            width: responsive.md ? 'calc(50% - 48px)' : 'calc(100% - 64px)'
         }}>
         {header()}
         <RightClickWrapper
--- src/pages/batchOperation/index.jsx
+++ src/pages/batchOperation/index.jsx
@@ -81,8 +81,9 @@ const BatchOperation = () => {
     const operationArea = <div
         className="operation-area"
         style={{
-            height: responsive.md ? 'calc(100% - 64px)' : 'calc(48%)',
-            width: responsive.md ? 'calc(50% - 48px)' : 'calc(100% - 32px)'
+            height: responsive.md ? 'calc(100% - 64px)' : 'calc(1000px - 64px)',
+            margin: responsive.md ? undefined : '0px 0px 32px 0px',
+            width: responsive.md ? 'calc(50% - 48px)' : 'calc(100% - 64px)'
         }}
     >
         {targetList}
